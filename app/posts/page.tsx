abaa5fb69a9ff9490980f6afff9f3c2fcf26098b
Han-Bin Song <thdgksq@naver.com>
2025-04-01 18:28:52 +0900

    DB로 부터 데이더 받아오는 기능 구현

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,5 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Link from 'next/link'
 import React from 'react'
 
-export default function PostsPage() {
-  return <div>PostsPage</div>
+async function getPost() {
+  const res = await fetch(
+    'http://127.0.0.1:8090/api/collections/posts/records',
+    { cache: 'no-store' }
+  )
+  const data = await res.json()
+
+  return data?.items as any[]
+}
+export default async function PostsPage() {
+  const posts = await getPost()
+  console.log(posts)
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      {posts?.map(post => {
+        return (
+          <PostItem
+            key={post.id}
+            post={post}
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+const PostItem = ({ post }: any) => {
+  const { id, title, created } = post || {}
+
+  return (
+    <Link href={`/posts/${id}`}>
+      <div>
+        <h3>{title}</h3>
+        <p>{created}</p>
+      </div>
+    </Link>
+  )
 }
