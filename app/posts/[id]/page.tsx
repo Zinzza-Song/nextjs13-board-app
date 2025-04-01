df298b1e29dbd5701b070b580fd4cb9fa2864cb3
Han-Bin Song <thdgksq@naver.com>
2025-04-01 18:48:36 +0900

    상세페이지 기능 구현

---
+++ app/posts/[id]/page.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+async function getPost(postId: string) {
+  const res = await fetch(
+    `http://127.0.0.1:8090/api/collections/posts/records/${postId}`,
+    { next: { revalidate: 10 } }
+  )
+  const data = await res.json()
+
+  return data
+}
+
+export default async function PostDetailPage({ params }: any) {
+  const { id } = await params
+  const post = await getPost(id)
+
+  return (
+    <div>
+      <h1>posts/{post.id}</h1>
+      <div>
+        <h3>{post.title}</h3>
+        <p>{post.created}</p>
+      </div>
+    </div>
+  )
+}
